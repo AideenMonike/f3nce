0e55cfad278dfaab0e170c691ec09fb075123cd2
AideenMonike <aidenl.monico@gmail.com>
2023-01-11 12:51:55 +1100

    New Assets, Starting code for target
    Code still WIP, skybox and target assets added.
    Timer or Coroutines for code???

---
+++ Assets/Scripts/TargetBehaviours.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+
+public class TargetBehaviours : MonoBehaviour
+{
+    public GameObject target;
+    private Rigidbody rb;
+    public float disappearTimer;
+    public float totalTimer;
+    public Vector3 origin;
+    public float spawnRadius;
+    private bool gameFinished;
+    private bool targetHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Timer targetTimer = new Timer(disappearTimer);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Foil")
+        {
+            if (!gameFinished)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+    private IEnumerator TargetSpawn()
+    {
+        transform.position = origin + Random.insideUnitSphere * spawnRadius;
+        yield return "Target Spawned";
+    }
+}
