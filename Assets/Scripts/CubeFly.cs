449dcaf7ed1def62e95d7a83deb3b12d385b88b6
AideenMonike <aidenl.monico@gmail.com>
2023-01-12 12:02:16 +1100

    Figuring out timer function
    For the targets mode I intend to have a timer to measure how long the game goes for. The timer function is something I have not sorted out yet, and so far I have been experimenting with coroutines and measuring framerate to find realtime. I will keep looking into this until I find a proper solution. Stopping the game when the timer runs out is simple enough though, only needing a conditional statement to check whether the value is at or below zero.

---
+++ Assets/Scripts/CubeFly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFly : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float disX = transform.position.x - target.transform.position.x;
+        float disY = transform.position.y - target.transform.position.y;
+        float disZ = transform.position.z - target.transform.position.z;
+        Vector3 force = new Vector3(-disX, -disY, -disZ);
+        rb.AddForce(force);
+    }
+}
--- Assets/Scripts/TargetBehaviours.cs
+++ Assets/Scripts/TargetBehaviours.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Timers;
@@ -6,38 +6,76 @@ using System.Timers;
 public class TargetBehaviours : MonoBehaviour
 {
     public GameObject target;
-    private Rigidbody rb;
+    //private Rigidbody rb;
     public float disappearTimer;
     public float totalTimer;
     public Vector3 origin;
     public float spawnRadius;
     private bool gameFinished;
     private bool targetHit;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        Timer targetTimer = new Timer(disappearTimer);
+        //rb = GetComponent<Rigidbody>();
+        //StartCoroutine(TargetHit());
+        gameFinished = false;
+        StartCoroutine(Timer());
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Foil")
         {
-            if (!gameFinished)
+            targetHit = true;
+            if (gameFinished)
             {
-
+                Debug.Log("timer done");
             }
             else
             {
-
+                
             }
         }
     }
+    
+    private IEnumerator Timer()
+    {
+        while (!gameFinished)
+        {
+            totalTimer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    /*
+    private IEnumerator TargetHit()
+    {
+        Debug.Log(Time.captureFramerate);
+        for (int i = 0; i <= Time.captureFramerate * disappearTimer; i++)
+        {
+            if (targetHit)
+            {
+                i = 0;
+                StartCoroutine(TargetSpawn());
+            }
+            else if (!targetHit && i == (Time.captureFramerate * disappearTimer))
+            {
+                StartCoroutine(TargetSpawn());
+                i = 0;
+            }
+        }
+        yield return null;
+    }
+    /*
+    private IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(disappearTimer);
+    }
+    
     private IEnumerator TargetSpawn()
     {
+        Debug.Log("A");
         transform.position = origin + Random.insideUnitSphere * spawnRadius;
-        yield return "Target Spawned";
+        yield return null;
     }
+    */
 }
