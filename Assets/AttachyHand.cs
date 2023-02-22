02cf56520ef4548b041817d81c7d106553675c0d
AideenMonike <aidenl.monico@gmail.com>
2023-02-23 10:03:12 +1100

    Further Foil Testing
    I recently discovered that while using the transform.position values of Unity, it does not use the Unity Physics engine, and therefore objects moved by transform do not have any velocity or force. This realisation is very important to making the foil bend, as it should be bending with a certain threshold of force even when not colliding with an object. Certainly, it would be easier to test the foil without usage of another collider. From here, I will be attempting to attach a separate GameObject that is the foil onto a hand in a separate scene, and apply the theoretical velocity onto the rigidbody velocity through the physics equation v = s/t. An additional note, while this is on the TargetTraining branch, it is not actually going to affect the targets very much. Henceforth I will be using the FoilPhysics branches for these experiments.

---
+++ Assets/AttachyHand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachyHand : MonoBehaviour
+{
+    public GameObject Connect;
+    public GameObject GrabObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GrabObject.transform.position = Connect.transform.position;
+    }
+}
---
+++ Assets/Scripts/FoilTrackingTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoilTrackingTest : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Transform foil;
+    public Collider foilBox;
+    public Transform handBox;
+    Vector3 foilVel;
+    Vector3 previousFoilPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        previousFoilPos = foil.position;
+        //StartCoroutine(AttachToHand());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foilVel = (foil.position - previousFoilPos) / Time.deltaTime; //using the physics formula v = u + at
+        previousFoilPos = foil.position;
+        /*
+        Debug.Log(foilVel);
+        Debug.Log("---");
+        */
+        Debug.Log(rb.velocity);
+        
+        rb.velocity = foilVel; 
+    }
+    private IEnumerator AttachToHand()
+    {
+        while (true)
+        {
+           foil.position = handBox.position - new Vector3(1, 0 , 1);
+            yield return null; 
+        }
+    }
+}
