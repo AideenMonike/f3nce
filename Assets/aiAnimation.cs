c378085746e7d6aace681373ab5ce9f84aead045
AideenMonike <aidenl.monico@gmail.com>
2023-03-21 21:07:15 +1100

    Beginning of AI Development - Animating the Model without VR Hardware
    The objective of this commit was to:
    - Import the model that will be used for the AI
    - Import the AI Plugin correctly into the Unity Project
    - Ensure the model is ready to be controlled by the computer
    
    The first two objectives were achieved with ease. Importing the plugin and model were fairly easy, just by using Unity's Package Manager. However, the third objective has yet to be completed. This is due to animations for the model. Currently I am working towards making the animations play when a certain condition is met, e.g. moving forward. However this is not working as expected, as the conditional triggers seem to not be activating. Further debugging is required, however the nature of the animator variables mean that output statements are much harder to manage, however that seems like the easiest way to see if the variable is affected and/or if the condition is being properly met.
    Overall, this is a good start to this branch, however much more work and research will be necessary for a polished final product.

---
+++ Assets/aiAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiAnimation : MonoBehaviour
+{
+    public float speedThreshold = 0.1f;
+    [Range(0,1)]
+    public float smoothing = 1;
+    private Animator anim;
+    private Vector3 previousPos;
+    public GameObject aiModel; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        previousPos = aiModel.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Speed
+        Vector3 modelSpeed = (aiModel.transform.position - previousPos) / Time.deltaTime;
+        modelSpeed.y = 0;
+        
+        Vector3 localModelSpeed = transform.InverseTransformDirection(modelSpeed);
+        previousPos = aiModel.transform.position;
+        
+        anim.SetBool("isMoving", localModelSpeed.magnitude > speedThreshold);
+        anim.SetFloat("direction", Mathf.Clamp(localModelSpeed.x, 0, 1));
+    }
+}
