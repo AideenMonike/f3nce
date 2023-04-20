34ef30c0ac6223d4e9777f0b0ba25c3e2be654b2
AideenMonike <aidenl.monico@gmail.com>
2023-04-21 08:16:27 +1000

    Maintaining Distance - Development of AI
    This push aimed to create a script that would allow the AI to maintain a certain distance away from the player.
    Many different methods were experimented with. This ranged from using cross/dot product to determine parallel nature, and calculating distance to walking direction. Eventually a more suitable solution was found: Vector3.MoveTowards()
    With this inbuilt method all that had to be done is make the target the player's position with a slightly adjusted x component to be in front of the player. The maintain distance function is almost complete, however a suitable formula for walkspeed is yet to be completed. The aim is for the speed to increase the most when the player is either very close or very far. This means that the function for walk speed should be a parabola, but I just haven't found the correct equation yet. Alternatively I could use an if statement but it may be more practical to use one formula.
    Overall, this push was a success.
    Lots of work and efficient modularisation has been employed, and the hardest function for this module has been completed.
    Also to note is that the Candice AI package has been deleted to save file space.

--- Assets/AIModelScripts/BehaviourScript.cs
+++ Assets/AIModelScripts/BehaviourScript.cs
@@ -13,24 +13,30 @@ public class BehaviourScript : MonoBehaviour
     public LayerMask whatIsGround;
     public LayerMask whatIsPlayer;
     //for general movement of ai
-    public Vector3 walkPoint;
-    bool walkPointSet;
+    private Vector3 playerPrevPosition;
+    public float distanceToMaintain;
     private float walkSpeed;
     private float walkDirection;
     private float distanceFromPlayer;
-    private float absDot;
-    private float dotDistance;
-    private Vector3 crossDistance;
+    private float vectorAngles;
+    private bool inBout;
     //Variables determining how the AI will attack
     public float attackRange;
     bool alreadyAttacked;
     public bool playerInAttackRange;
 
+    void OnCollisionEnter(Collision other)
+    {
+        if (gameObject.tag == "boutBoundary") inBout = true;
+        else inBout = false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").transform;
+        player = GameObject.Find("Capsule").transform;
         agent = GetComponent<NavMeshAgent>();
+        playerPrevPosition = player.position;
     }
 
     // Update is called once per frame
@@ -44,15 +50,41 @@ public class BehaviourScript : MonoBehaviour
         }
     }
 
-    private void MaintainDistance() {
+    private void MaintainDistance() 
+    {
         distanceFromPlayer = Vector3.Distance(transform.position, player.position);
-        walkSpeed = distanceFromPlayer / 10;
-        //calculates whether model is parallel to player, restrained by bout boundaries
-        if (Mathf.Approximately(Vector3.Angle(transform.position, player.position), 0 | 180))
-        {
+        walkSpeed = 2 * Mathf.Pow(distanceFromPlayer - 3, 2) + 0.5f;
+        Debug.Log(walkSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, player.position - new Vector3 (5, 0, 0), walkSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        /*
+        distanceFromPlayer = transform.position.x - player.position.x;
+        
+        vectorAngles = Vector3.Angle(transform.position, player.position);
 
+        walkDirection = player.position.x - playerPrevPosition.x;
+        if (walkDirection >= 0) {
+            walkDirection = 1;
         }
+        else {
+            walkDirection = -1;
+        }
+        switch (walkDirection)
+        {
+            case -1:
+                walkSpeed = Mathf.Abs((distanceFromPlayer * -2) + 5);
+                break;
+            case 1:
+                walkSpeed = Mathf.Abs((distanceFromPlayer * 2) - 5);
+                break;
+        }
+        
         transform.position = transform.position + new Vector3(walkDirection * walkSpeed * Time.deltaTime, 0, 0);
+
+        if ((175 <= vectorAngles && vectorAngles <= 185 || 5 <= vectorAngles && vectorAngles <= 15) && inBout) {
+            transform.position = transform.position + new Vector3 (0, 0, walkSpeed * Time.deltaTime);
+        }
+        */
     }
     private void ChaseIntoAttack() {
         
@@ -61,3 +93,5 @@ public class BehaviourScript : MonoBehaviour
 
     }
 }
+
+
