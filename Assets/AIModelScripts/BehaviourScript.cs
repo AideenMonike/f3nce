21e269fc5fccf7ed36cc8deb127655736544c5d8
AideenMonike <aidenl.monico@gmail.com>
2023-05-01 20:43:04 +1000

    Attacking and Lunge & Optimising Moving Back into Bout - Development of AI
    The objective of this push was to successfully implement the use of chasing into attack and lunging functions of the AI. This mainly involved implementing a theoretical solution that works and has been tested.
    Using parameters for the range needed to chase or lunge,  conditions would be checked within the FixedUpdate() module. If the chase condition is met, the model would rush towards the player. This simply updated the transform position over time using Time.deltaTime, and works flawlessly. Upon crossing the threshold to lunge, the animator variable would be set to true, and a boolean which disables other functions would be set true. An event is called upon once the lunge animation is complete, unlocking other modules and allowing the capacity for movement. A timer will also start on a separate thread that prohibits multiple attacks within a short period of time. All modules work as intended.
    The move back function was also optimised to work under more conditions. An issue that arose was the model swapping positions too quickly between the back or side of the bout and the player. To solve this issue, a float variable that would be counted down with Time.deltaTime. The raycast will then only be called once the timer had reset. This fixed the problem and allowed the model to move back into the bout properly.
    Overall this push was a success, with no created issues and two subroutines successfully implemented. Next will be UI, and once the foil physics are complete importing the VR model in and working on the Parry subroutine.

--- Assets/AIModelScripts/BehaviourScript.cs
+++ Assets/AIModelScripts/BehaviourScript.cs
@@ -9,7 +9,7 @@ public class BehaviourScript : MonoBehaviour
     public NavMeshAgent agent;
     public Animator anim;
     //Player Transform 
-    public Transform player;
+    public GameObject player;
     // Layermasks to identify ground and player
     public LayerMask whatIsPlayer;
     public LayerMask whatIsBout;
@@ -24,8 +24,9 @@ public class BehaviourScript : MonoBehaviour
     private bool backToArena;
     // Variables determining how the AI will attack
     public float attackRange;
-    private float lungeRange; // to be measured with dummy arm
-    private bool alreadyAttacked;
+    public float lungeRange; // to be measured with dummy arm
+    private bool lungeLock = false;
+    private bool alreadyAttacked = false;
     public float attackCooldownTime;
     private bool playerInAttackRange;
     private bool playerInLungeRange;
@@ -41,7 +42,7 @@ public class BehaviourScript : MonoBehaviour
     {
         if (other.gameObject.tag == "AIBoutLimit") {
             returnToField = false;
-            iTimerField = 1f;
+            iTimerField = 0.5f;
             Debug.Log("return " + returnToField);
         }   
     }
@@ -50,7 +51,6 @@ public class BehaviourScript : MonoBehaviour
     // Instantiates components and identifies player, and gets the dummy's previous position
     void Start()
     {
-        player = GameObject.Find("Capsule").transform;
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
     }
@@ -64,54 +64,56 @@ public class BehaviourScript : MonoBehaviour
         // Checks if player is within attack range, elsewise tries to maintain distance or move back to the middle of the arena
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
         playerInLungeRange = Physics.CheckSphere(transform.position, lungeRange, whatIsPlayer);
-
-        // Need condition for parry, possibly based on position of foil?
-        if (!alreadyAttacked) 
+        if (!lungeLock)
         {
-            if (playerInLungeRange) {
-                Lunge();
+            // Need condition for parry, possibly based on position of foil?
+            if (!alreadyAttacked) 
+            {
+                if (playerInLungeRange && !lungeLock) {
+                    Lunge();
+                    Debug.Log("Lunge Called");
+                }
+                else if (playerInAttackRange && !playerInLungeRange) {
+                    ChaseIntoAttack();
+                }
             }
-            else if (playerInAttackRange && !playerInLungeRange) {
-                ChaseIntoAttack();
+            
+            // Conditional statements to determine whether the AI will move into the middle of the field or if it will attempt to follow the player
+            if (!playerInAttackRange && inBout && returnToField) {
+                MaintainDistance();
             }
+            else if (!playerInAttackRange && inBout && !returnToField) {
+                MoveBack();
+            }
+            Debug.DrawRay(transform.position + new Vector3 (0, 1, 0), Vector3.down, Color.blue, 0.1f);
         }
-        
-        // Conditional statements to determine whether the AI will move into the middle of the field or if it will attempt to follow the player
-        if (!playerInAttackRange && inBout && returnToField) {
-            MaintainDistance();
-        }
-        else if (!playerInAttackRange && inBout && !returnToField) {
-            MoveBack();
-        }
-        Debug.DrawRay(transform.position + new Vector3 (0, 1, 0), Vector3.down, Color.blue, 0.5f);
     }
 
     private void MaintainDistance() 
     {
         // Calculates the walk speed of the dummy based on distance. Will also ensure the walkspeed is always at least 1 for moving on the Z axis
-        distanceFromPlayer = Vector3.Distance(transform.position, player.position);
+        distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
         walkSpeed = Mathf.Round(2 * Mathf.Pow(distanceFromPlayer - distanceToMaintain, 2));
         if (walkSpeed <= 0) walkSpeed = 1;
         
         // Moves dummy based upon player's position. Will also lock it's Y axis movement to 0 so it's always grounded
-        transform.position = Vector3.MoveTowards(transform.position, player.position - new Vector3 (distanceToMaintain, 0, 0), walkSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position - new Vector3 (distanceToMaintain, 0, 0), walkSpeed * Time.deltaTime);
         transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
     private void MoveBack()
     {
         // Shoots a raycast backwards to see if it has hit the back of the arena
-        backToArena = Physics.Raycast(transform.position, Vector3.left, 0.75f, backOfArena);
+        if (iTimerField >= 0.5f) backToArena = Physics.Raycast(transform.position, Vector3.left, 0.75f, backOfArena);
         Debug.DrawRay(transform.position, Vector3.left, Color.red, 0.5f);
 
         // Decides whether it has to move to the middle or to the middle and away from the back of the arena
         if (backToArena)
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3 (player.position.x - 0.5f, 0, 0), 3 * Time.deltaTime);
-            Debug.Log("raycast hit back of arena");
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3 (player.transform.position.x - 0.5f, 0, 0), 2 * Time.deltaTime);
         }
         else
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3 (player.position.x - distanceToMaintain, 0, 0), 2 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3 (player.transform.position.x - distanceToMaintain, 0, 0), 2 * Time.deltaTime);
         }
 
         // Timer field is to determine how long the AI will move away from the edge of the arena. 
@@ -125,21 +127,36 @@ public class BehaviourScript : MonoBehaviour
     }
     // Glorified move towards but speedy
     private void ChaseIntoAttack() {
-        
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position - new Vector3 (0.5f, 0, 0), 3 * Time.deltaTime);
+        transform.position = new Vector3 (transform.position.x, 0, transform.position.z);
     }
     // need to wrap hitbox around animation component
     private void Lunge() {
-        
+        anim.SetBool("withinLungeRange", true);
+        lungeLock = true;
+        alreadyAttacked = true;
+        // Get animation state: if done start coroutine, if not wait
+        // can afford stopping THE ENTIRE SCRIPT (i think) if animation is still playing
+        // find variable speed for animation
+        StartCoroutine(AttackCooldown(attackCooldownTime));
+    }
+    private void Unlock() {
+        anim.SetBool("withinLungeRange", false);
+        lungeLock = false;
+        Debug.Log("Unlock called");
     }
     // i have no fucking clue how to do this ((:
     private void Parry() {
 
     }
 
-    private IEnumerator AttackCooldown()
+    private IEnumerator AttackCooldown(float timer)
     {
-        while (attackCooldownTime > 0) attackCooldownTime -= Time.deltaTime;
+        while (timer > 0) {
+            timer -= Time.deltaTime;
+            Debug.Log(timer);
+            yield return new WaitForEndOfFrame();
+        }
         alreadyAttacked = false;
-        yield return null;
     }
 }
