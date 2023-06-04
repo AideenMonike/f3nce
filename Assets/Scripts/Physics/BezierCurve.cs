509fe2633d10d00e69404400272917666aba8b9c
AideenMonike <aidenl.monico@gmail.com>
2023-06-04 19:04:12 +1000

    Foil Bend Conditions and Bezier Curve
    The objective of this push was to apply smoothing to the bezier curve, and apply conditions for the foil to bend properly. This push was a mixed success.
    Smoothing was achieved with relative ease using the Lerp (Linear interpolation) function on vectors. This allowed a variable to control the speed at which the control point approached it's final destination, allowing the bend to apply smoothly to the foil. In this regard, the push was a success.
    However, the conditional aspect of the flick is a major issue at the moment. A video that was filmed and uploaded to the issues tab on the repo demonstrates how the bend is caused by the inertia of the tip, essentially showing that the tip moving is what is causing the bend. Previously, attempts were made to try make the foil bend around the location of the tip based on the difference in velocities of the tip and base, but this is no longer an option as the tip has to be the one that is moved. As a result the controlpoint also will not have to be adjusted as much.
    Further exploration will be made with sudden changes in acceleration, most likely deriving the velocity equation v = u + at to calculate accleration, and cause the tip to move accordingly.
    Overall, this push was somewhat of a mixed success, but the current issue is very large.

--- Assets/Scripts/Physics/BezierCurve.cs
+++ Assets/Scripts/Physics/BezierCurve.cs
@@ -1,77 +1,109 @@
 ﻿using UnityEngine;
 
-//[ExecuteInEditMode]
 public class BezierCurve : MonoBehaviour
 {
     // Vector3, Transforms and GameObjects variables. Information for the base, tip, control points and previous positions
     public GameObject tip, _base;
     [SerializeField] private Vector3 midPoint;    
     private Vector3 prevMidPoint;
-    [SerializeField] private Vector3 controlPoint;
+    [SerializeField] private Vector3 controlPoint, prevControlPoint;
     private Vector3 tipPrevPos, basePrevPos;
     // Float variables to control the position and speed of the control point
     private float lerpMult;
-    public float smoothing;
+    [Range (0, 1)] public float smoothing;
     // Theoretical velocities of the tip
-    [SerializeField] private Vector3 tipVel;
+    [SerializeField] private Vector3 tipVel, baseVel;
     private Vector3 prevTipVel;
+    private bool noBendFoil;
     // Array of bones for the foil, positions calculated by the bezier curve and array-related variable;
     [SerializeField] private Transform[] bones;
     private int boneCount;
-    private int posUpperBound;
-    private Vector3[] positions;
-    public LineRenderer line;
+    private int posLineUpperBound, posCurveUpperBound;
+    private Vector3[] curvePositions, linePositions;
+
+    public float velThreshold;
 
     public void Start()
     {
+        // Sets all the variables for the arrays used
         boneCount = bones.Length;
-        positions = new Vector3[boneCount];
-        posUpperBound = positions.GetUpperBound(0);
-        positions[posUpperBound] = tip.transform.position;
 
-        // Gets the starting position of the tip (& base) of the foil
+        linePositions = new Vector3[boneCount];
+        posLineUpperBound = linePositions.GetUpperBound(0);
+        linePositions[posLineUpperBound] = tip.transform.position;
+        
+        curvePositions = new Vector3[boneCount];
+        posCurveUpperBound = curvePositions.GetUpperBound(0);
+        curvePositions[posCurveUpperBound] = tip.transform.position;
+
+        // Gets the starting position of various positions and Vector3s
         tipPrevPos = tip.transform.position;
         basePrevPos = _base.transform.position;
         prevTipVel = tipVel;
-
         prevMidPoint = (_base.transform.position + tip.transform.position) / 2;
+        prevControlPoint = controlPoint;
     }
 
     public void Update()
     {
-        tipVel = (tipPrevPos - tip.transform.position) / Time.deltaTime;
-        lerpMult = Mathf.Lerp(Mathf.Abs(prevTipVel.magnitude) / 2, Mathf.Abs(tipVel.magnitude) / 2, smoothing);
+        CalcPointVelocities();
         CalcControlPoint();
+        noBendFoil = FloatValueCloseEnough(tipVel.magnitude, baseVel.magnitude, 5f);
         SortPoints();
+    }
+    
+    // Calculates theoretical velocities using the equation v = s/t, where s is the displacement and t is time.
+    // ALso calculates the control point's position using lerpMult (lerp multiplier).
+    private void CalcPointVelocities()
+    {
+        //lerpMult = 0;
+        tipVel = (tip.transform.position - tipPrevPos) / Time.deltaTime;
+        baseVel = (_base.transform.position - basePrevPos) / Time.deltaTime;
+        lerpMult = Mathf.Abs(tipVel.magnitude) / 100;
+
         tipPrevPos = tip.transform.position;
         prevTipVel = tipVel;
+        basePrevPos = _base.transform.position;
     }
 
     // Need to lock midpoint/control point when moving along foil axis
     // something to do with local axis???
-    // Also need to smooth the movement of the control point
-    void CalcControlPoint()
+
+    // FOR SMOOTHING
+    // All it required was a lerp from it's previous control point to it's current. Since LerpUnclamped
+    // Was moreso for extrapolition over an animation curve, using Lerp actually affected how it ran.
+    // A vector projection was also used so that the control point would be close to above the midpoint of the foil.
+    private void CalcControlPoint()
     {
         midPoint = (_base.transform.position + tip.transform.position) / 2;
-        controlPoint = Vector3.LerpUnclamped(midPoint, prevMidPoint, 1 * lerpMult);
+        controlPoint = Vector3.LerpUnclamped(midPoint, prevMidPoint, lerpMult);
+        controlPoint = Vector3.Lerp(prevControlPoint, Vector3.Project(midPoint, controlPoint), smoothing);
+        
         prevMidPoint = midPoint;
+        prevControlPoint = controlPoint;
     }
 
-    // Maybe add a conditional statement dictating that the bezier calculations happen after a huge difference in velocity
+    // Calculates t so it will always be an evenly distributed fraction.
+    // All positions are already in the global space.
     private void SortPoints()
     {
         for (int i = 1; i < bones.Length + 1; i++)
         {
             float t = i / (float)boneCount;
-            positions[i - 1] = CalculateBezierCurvePoint(t, _base.transform.position, tip.transform.position, controlPoint);
+            // IF ELSE FOR LINEAR OR QUADRATIC
+            curvePositions[i - 1] = CalculateBezierCurvePoint(t, _base.transform.position, tip.transform.position, controlPoint);
+            linePositions[i - 1] = CalculateBezierLinePoint(t, _base.transform.position, tip.transform.position);
         }
-        line.SetPositions(positions);
-
-        for (int i = 0; i < positions.Length; i++)
+    
+        for (int i = 0; i < curvePositions.Length; i++)
         {
-            bones[i].position = positions[i];
+            bones[i].position = curvePositions[i];
         }
     }
+    
+    // Calculates positions along a curve using the quadratic bezier curve equation. Variable t is clamped within 0 to 1.
+    // Alternatively, a cubic bezier curve can be used, but that would necessitate another control point, which for this
+    // feature is unnecessary.
     private Vector3 CalculateBezierCurvePoint(float t, Vector3 point1, Vector3 point2, Vector3 midP)
     {
         float tt = t * t;  
@@ -80,5 +112,23 @@ public class BezierCurve : MonoBehaviour
         float _2ut = 2 * u * t;
 
         return ((point1 * uu) + (_2ut * midP) + (tt * point2));
-    } 
+    }
+
+    private Vector3 CalculateBezierLinePoint(float t, Vector3 point1, Vector3 point2)
+    {
+        float u = 1 - t;
+
+        return (u * point1) + t * point2;
+    }
+
+    public static bool FloatValueCloseEnough(float a, float b, float maxValueApart)
+    {
+        if ((a - b) <= maxValueApart) return true;
+        else return false;
+    }
+
+    /* public static bool Vector3CloseEnough(Vector3 a, Vector3 b, Vector3 maxValueApart)
+    {
+        if ((a - b) <= maxValueApart) return true;
+    } */
 }
